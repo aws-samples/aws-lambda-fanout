bf062822f8fec0ef87be131e5da2d1dea84e1958
Lauren <Lauren.Wang@nordstrom.com>
2017-05-01 16:27:00 -0700

    script for adding target streams in a fixed account

---
+++ register-student-sandbox-stream.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+if [ -z $1 ] || [ -z $STAGE ] || [ -z $REGION ]
+    then
+        echo "Usage: register-student-sandbox-stream <lanid>"
+        echo "NOTE! The following environment variables must be set:"
+        echo "STAGE, name of the workshop stage."
+        echo "REGION, workshop region."
+        exit 1
+fi
+
+RETAIL_STREAM_NAME=`aws kinesis list-streams | sed -n "s/^.*\"\(.*\)\".*/\1/p" | grep ${STAGE}RetailStream`
+
+if [ -z $RETAIL_STREAM_NAME ]
+    then
+        echo "ERROR: Did not find RetailStream for environment $STAGE."
+        exit 2
+    else
+        echo "Finding ARN for stream $RETAIL_STREAM_NAME ..."
+fi
+
+RETAIL_STREAM_ARN=`aws kinesis describe-stream --stream-name $RETAIL_STREAM_NAME | grep StreamARN | sed -n "s/^.*\(arn\:aws\:kinesis\:.*RetailStream\).*/\1/p"`
+
+if [ -z $RETAIL_STREAM_ARN ]
+    then
+        echo "ERROR: Did not find RetailStream ARN for $RETAIL_STREAM_NAME."
+        exit 3
+    else
+        echo "Invoking './fanout' to attach stream $RETAIL_STREAM_ARN ..."
+fi
+
+./fanout register kinesis --source-arn $RETAIL_STREAM_ARN --id $1  --destination-region $REGION --active true --parallel false --destination-role-arn arn:aws:iam::468543742856:role/${1}StreamWriter --destination arn:aws:kinesis:us-west-2:468543742856:stream/${1}Stream
+
+if [ $? ]
+    then
+        echo "DONE. Student stream attached."
+    else
+        echo "ERROR. See output from './fanout' above."
+        exit 4
+fi

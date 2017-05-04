8232c7df63bbd89dcaef561bded196d4b4719dd1
Gregory Smith, II <Greg.W.Smith@nordstrom.com>
2017-05-03 17:51:09 -0700

    Using environment to control behavior when deployed and writes to DynamoDB.

--- slackbot/handler.js
+++ slackbot/handler.js
@@ -1,29 +1,91 @@
 const querystring = require('querystring');
+const AWS = require('aws-sdk');
+
+const streamArnRegEx = /^arn:aws:kinesis:[a-z0-9\-]*:[0-9]{12}:stream\/(.*Stream)$/;
+const roleArnRegEx = /^arn:aws:iam::[0-9]{12}:role\/(.*)StreamWriter$/;
+
+AWS.config.update({
+    region: process.env.CORE_STREAM_REGION
+});
+
+function validateRoleArn(roleArn) {
+    return !(roleArn.match(roleArnRegEx) === null);
+}
+
+function validateKinesisArn(streamArn) {
+    return !(streamArn.match(streamArnRegEx) === null);
+}
+
+function getRetailStreamArn() {
+    return `arn:aws:kinesis:${process.env.CORE_STREAM_REGION}:${process.env.CORE_STREAM_ACCOUNT}:stream/${process.env.HELLO_RETAIL_STAGE}RetailStream`
+}
+
+function extractUniqueIdFromRoleArn(roleArn) {
+    return roleArn.match(roleArnRegEx)[1];
+}
+
+function extractStreamName(streamArn) {
+    return streamArn.match(streamArnRegEx)[1];
+}
+
+function registerStreamForFanOut(roleArn, streamArn) {
+    const dynamoDb = new AWS.DynamoDB();
+    const retailStreamArn = getRetailStreamArn();
+    const id = extractUniqueIdFromRoleArn(roleArn);
+    const destination = extractStreamName(streamArn);
+
+    const item = {
+        'active': { 'BOOL': true },
+        'collapse': { 'S': 'none' },
+        'destination': { 'S': destination },
+        'id': { 'S': id },
+        'parallel': { 'BOOL': false },
+        'region': { 'S': process.env.CORE_STREAM_REGION },
+        'role': { 'S': roleArn },
+        'sourceArn': { 'S': retailStreamArn },
+        'type': { 'S': 'kinesis' } 
+    };
+
+    console.log(item);
+
+    dynamoDb.putItem({
+        'TableName': process.env.FANOUT_TABLE_NAME,
+        'Item': item
+    }, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+    });
+}
 
 module.exports = {
     hookstream: function (event, context, callback) {
-        console.log(event, context)
-
-        const query = querystring.parse(event.body)
-
-        console.log(query.user_name)
-        console.log(query.text)
-
-        const args = query.text.split(' ')
+        const query = querystring.parse(event.body);
+        const args = query.text.split(' ');
         let message = 'WOAH!';
 
-        if(args.length != 2) {
+        // Validate environment:
+        if(!process.env.CORE_STREAM_REGION) {
+            // CORE_STREAM_REGION is the AWS region for the workshop
+            message = 'Invalid deployment of this Lambda: CORE_STREAM_REGION must be set.'
+        } else if(!process.env.CORE_STREAM_ACCOUNT) {
+            // CORE_STREAM_ACCOUNT account number for source Retail Stream
+            message = 'Invalid deployment of this Lambda: CORE_STREAM_ACCOUNT must be set.'
+        } else if(!process.env.HELLO_RETAIL_STAGE) {
+            // HELLO_RETAIL_STAGE set to the stage name for the workshop, e.g. 'workshop050217' or 'austin'
+            message = 'Invalid deployment of this Lambda: HELLO_RETAIL_STAGE must be set.'
+        } else if(args.length != 2) {
             message = 'Invalid arguments. Expected: <writer-role-arn> <stream-arn>'
         } else {
             const roleArn = args[0];
             const streamArn = args[1];
 
-            if(!roleArn.match(/^arn:aws:iam::[0-9]{12}:role\/.*StreamWriter$/)) {
+            if(!validateRoleArn(roleArn)) {
                 message = `Expected the *StreamWriter role ARN, but saw ${roleArn} instead.`
-            } else if (!streamArn.match(/^arn:aws:kinesis:[a-z0-9\-]*:[0-9]{12}:stream\/.*Stream$/)) {
+            } else if (!validateKinesisArn(streamArn)) {
                 message = `Expected the *Stream ARN, but saw ${streamArn} instead.`
             } else {
-                message = `Connecting Hello-Retail Stream to ${streamArn} using ${roleArn}.`
+                message = `Thanks ${query.user_name}! We're connecting the ${process.env.HELLO_RETAIL_STAGE} core Retail Stream to ${streamArn} using ${roleArn}.`
+                registerStreamForFanOut(roleArn, streamArn);
             }
         }
 
@@ -32,15 +94,6 @@ module.exports = {
             statusCode: 200,
             headers: {  },
             body: message
-        })
-    },
-    unhookstream: function(event, context, callback) {
-        console.log(event, context)
-        callback(null, {
-            isBase64Encoded: false,
-            statusCode: 200,
-            headers: {  },
-            body: "Hello from Lambda!"
         })
     }
 }

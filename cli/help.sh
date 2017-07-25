d34a639c6d90d1935c241b517f5d62049a187076
Eric Sherman <eric.sherman@pgi.com>
2017-07-25 09:57:22 -0400

    adds --apend-newlines target option to append newlines to DynamoDB records

--- cli/help.sh
+++ cli/help.sh
@@ -101,6 +101,7 @@ function helpObjectProperties {
   echo "  --parallel <flag>: processes multiple messages in parallel for efficiency" 1>&2
   echo "  --convert-ddb <flag>: for Amazon DynamoDB Streams messages, converts the DDB objects to plain JavaScript objects" 1>&2
   echo "  --deaggregate <flag>: for Amazon Kinesis Streams messages, deserializes KPL (protobuf-based) messages" 1>&2
+  echo "  --append-newlines <flag>: for Amazon DynamoDB Streams messages, append a newline to the end of each record" 1>&2
 }
 
 function helpTargetParams {
